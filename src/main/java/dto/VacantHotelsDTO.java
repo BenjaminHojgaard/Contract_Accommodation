8ca71462f63bcaa4d7b7235e4e93dfdcfa1d6087
Highyard <benjaminoest@hotmail.com>
2020-11-29 15:28:32 +0100

    added ctors, getters, setters for VacantDTOs

--- src/main/java/dto/VacantHotelsDTO.java
+++ src/main/java/dto/VacantHotelsDTO.java
@@ -14,4 +14,37 @@ public class VacantHotelsDTO {
      * indicates how many people
      */
     private int numberOfGuests;
+
+    public VacantHotelsDTO(String city, Date date, int numberOfGuests) {
+        this.city = city;
+        this.date = date;
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public VacantHotelsDTO() {
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
 }
--- src/main/java/dto/VacantRoomsDTO.java
+++ src/main/java/dto/VacantRoomsDTO.java
@@ -14,4 +14,37 @@ public class VacantRoomsDTO {
      * indicates how many people
      */
     private int numberOfGuests;
+
+    public VacantRoomsDTO(int hotelNumber, Date date, int numberOfGuests) {
+        this.hotelNumber = hotelNumber;
+        this.date = date;
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public VacantRoomsDTO() {
+    }
+
+    public int getHotelNumber() {
+        return hotelNumber;
+    }
+
+    public void setHotelNumber(int hotelNumber) {
+        this.hotelNumber = hotelNumber;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
 }
