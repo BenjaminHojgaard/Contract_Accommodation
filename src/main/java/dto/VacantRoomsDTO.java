f5e2b687bac08030dc78790f2a72a1fc9938f894
Highyard <benjaminoest@hotmail.com>
2020-11-29 15:19:33 +0100

    added VacantRoomsDTO and VacantHotelsDTO

---
+++ src/main/java/dto/VacantRoomsDTO.java
@@ -0,0 +1,17 @@
+package dto;
+
+import java.util.Date;
+
+public class VacantRoomsDTO {
+
+    /** indicates what hotel number the room is at
+    */
+    private int hotelNumber;
+    /** indicates what date the room is resarved
+    */
+    private Date date;
+    /**
+     * indicates how many people
+     */
+    private int numberOfGuests;
+}
